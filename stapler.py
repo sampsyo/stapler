7782e4f6d2237604c613458803d8c46c3560e47a
Adrian Sampson <adrian@radbox.org>
2011-08-23 10:14:43 -0700

    read content from the filesystem

--- stapler.py
+++ stapler.py
@@ -3,6 +3,15 @@ import sys
 import os
 import jinja2
 import jinja2.sandbox
+import re
+import yaml
+
+INDEX_FILENAME = 'index.html'
+BUILD_DIR = '_out'
+CONTENT_DIR = 'items'
+STATIC_DIR = 'media'
+TEMPLATES_DIR = 'templates'
+SEPORATOR_RE = re.compile(r'\s*^---\n', re.MULTILINE)
 
 Route = namedtuple('Route', ['path', 'template', 'block'])
 
@@ -27,16 +36,69 @@ def _parse_config(f):
 
     return routes
 
+class Item(object):
+    def __init__(self, attrs, body):
+        self.attrs = attrs
+        self.body = body
+
+    def __str__(self):
+        return str(self.body)
+
+    def __unicode__(self):
+        return unicode(self.body)
+
+    @classmethod
+    def from_string(cls, s):
+        m = SEPORATOR_RE.match(s)
+        if not m:
+            # No attributes.
+            attrs = ''
+            body = s
+        else:
+            rest = s[m.end():]
+            m = SEPORATOR_RE.search(rest)
+            if not m:
+                # No body.
+                attrs = rest
+                body = ''
+            else:
+                attrs = rest[:m.start()]
+                body = rest[m.end():]
+        return cls(yaml.load(attrs) or {}, body)
+
+class FSSource(object):
+    def __init__(self, path):
+        self.path = path
+
+    def _items(self):
+        for dirpath, dirnames, filenames in os.walk(self.path,
+                                                    followlinks=True):
+            for filename in filenames:
+                if filename.startswith('.'):
+                    continue
+                filepath = os.path.join(dirpath, filename)
+                with open(filepath) as f:
+                    content = f.read()
+                yield Item.from_string(content)
+
+    def __iter__(self):
+        return self._items()
+
 class Site(object):
     def __init__(self, fn):
         with open(fn) as f:
             self.routes = _parse_config(f)
+
         self.basedir = os.path.dirname(os.path.abspath(fn))
-        self.outdir = os.path.join(self.basedir, '_out')
+        tmpldir = os.path.join(self.basedir, TEMPLATES_DIR)
+        self.outdir = os.path.join(self.basedir, BUILD_DIR)
+        contentdir = os.path.join(self.basedir, CONTENT_DIR)
+        self.staticdir = os.path.join(self.basedir, STATIC_DIR)
 
         self.env = jinja2.sandbox.SandboxedEnvironment(
-            loader=jinja2.FileSystemLoader(self.basedir)
+            loader=jinja2.FileSystemLoader(tmpldir)
         )
+        self.source = FSSource(contentdir)
 
     def render(self):
         if not os.path.isdir(self.outdir):
@@ -46,7 +108,7 @@ class Site(object):
             tmpl = self.env.get_template(route.template)
             path = route.path
             if path.endswith('/'):
-                path += 'index.html'
+                path += INDEX_FILENAME
             if path.startswith('/'):
                 path = path[1:]
             outfn = os.path.join(self.outdir, path)
